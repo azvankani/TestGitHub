b36b8d6c4b876cfc2b565cccedd886668cd1f5fe
azvankani <azvankani@gmail.com>
2017-08-29 19:43:09 -0400

    feature added

--- src/github/TestGitHub.java
+++ src/github/TestGitHub.java
@@ -4,6 +4,6 @@ public class TestGitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("Github Success!");
-		System.out.println("blah!");
+		System.out.println("hjhjhh");
 	}
 }
